fa895b660e30f1f19bc13f5c6705fc15a66212c5
coatless <balamut2@illinois.edu>
2015-03-16 23:40:35 -0500

    modified table name to include schema

--- db_build/copy_statement.R
+++ db_build/copy_statement.R
@@ -3,4 +3,4 @@ file.dir = "F:\\Documents\\GitHub\\nasacms\\fia"
 temp = list.files(path = file.dir, pattern="*.CSV")
 tempnocsv = gsub(".CSV","", temp)
 
-cat(print(paste("COPY ",tempnocsv," FROM '",paste0(file.dir,"\\",temp),"' DELIMITER ',' CSV HEADER", sep="", collapse="\n")))
+cat(print(paste("COPY FIA.",tempnocsv," FROM '",paste0(file.dir,"\\",temp),"' DELIMITER ',' CSV HEADER;", sep="", collapse="\n")))
