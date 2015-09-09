6ac7871cf989ac714c75ac95b3d6186163b28594
coatless <balamut2@illinois.edu>
2015-09-09 15:50:43 -0500

    init safe r commit

---
+++ fiadb/R/fiadb_package.R
@@ -0,0 +1,28 @@
+#' fiadb Package
+#'
+#' fiadb
+#' 
+#' @details 
+#' \tabular{ll}{
+#' Package: \tab fiadb\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.0.1\cr
+#' Date: \tab 2015-05-26\cr
+#' License: \tab MIT + file LICENSE\cr
+#' }
+#' Some details should be added  
+#' 
+#' @author
+#' James Balamuta \email{balamut2@@illinois.edu},
+#' Bo Li \email{libo@illinois.edu}
+#'
+#' Bo Li \email{libo@illinois.edu}
+#' @name fiadb-package
+#' @docType package
+#' @useDynLib fiadb
+#' @importFrom Rcpp evalCpp
+#' @import ggplot2 grid scales
+#' @exportPattern ^[[:alpha:]]+
+#' @examples
+#' # Fill in at a later time
+NULL
